77384dc5537367078c7dbdc90f03bb6fa1decebd
Roman Motyka <r.motyka@cctechnology.pl>
2017-10-30 09:45:14 +0100

    E13

--- E013.fsx
+++ E013.fsx
@@ -1,16 +1,19 @@
-let isEven n =
-    n % 2 = 0
+open System
+open System.IO
+open Microsoft.FSharp.Math
 
-let collatz input  =
-    let rec aux n counter = 
-        printfn "%A" n
-        if n = 1 then
-            counter
-        else
-            if isEven n then
-                aux (n / 2) (counter + 1)
-            else
-                aux (3 * n + 1) (counter + 1)
-    aux input 1
+let addBigint x y =
+    bigint.Add(x, y)
 
-collatz 13
+let calcSum () =
+    let restult = 
+        File.ReadAllLines("resources/E13data.txt")
+        |> Array.filter(fun x -> not (String.IsNullOrWhiteSpace(x)))
+        |> Array.map(fun x -> x.Trim())
+        |> Array.map(fun x -> bigint.Parse(x))
+        |> Array.sum
+        |> sprintf "%A"
+    restult.[0 .. 9]
+
+
+calcSum()
---
+++ E014.fsx
@@ -0,0 +1,15 @@
+let isEven n =
+    n % 2 = 0
+
+let collatz input  =
+    let rec aux n counter = 
+        if n = 1 then
+            counter
+        else
+            if isEven n then
+                aux (n / 2) (counter + 1)
+            else
+                aux (3 * n + 1) (counter + 1)
+    aux input 1
+
+collatz 13
